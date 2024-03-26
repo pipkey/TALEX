a25917cce8ee30c911eb5ba38dbfd0f1fb6a4aab
pipkey <galagen2014@gmail.com>
2024-03-26 23:08:06 +0300

    slider_form_visual

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-        center: ["55°41'40.4", "38°00'01.2"], // Координаты вашего местоположения
-        zoom: 12 // Масштаб карты
-    });
+  var myMap = new ymaps.Map("map", {
+    center: ["55°41'40.4", "38°00'01.2"], // Координаты вашего местоположения
+    zoom: 12, // Масштаб карты
+  });
 
-    var myPlacemark = new ymaps.Placemark(["55°41'40.4", "38°00'01.2"], {
-        hintContent: 'Место, которое вам нужно',
-        balloonContent: 'Дополнительная информация'
-    });
+  var myPlacemark = new ymaps.Placemark(["55°41'40.4", "38°00'01.2"], {
+    hintContent: "Место, которое вам нужно",
+    balloonContent: "Дополнительная информация",
+  });
 
-    myMap.geoObjects.add(myPlacemark);
+  myMap.geoObjects.add(myPlacemark);
 });
+
+// swiper
