4e0083981c3f509ff1a481f8b815b8a3e759bc3f
pipkey <galagen2014@gmail.com>
2024-04-01 23:03:15 +0300

    whatWeDo

--- js/index.js
+++ js/index.js
@@ -12,4 +12,39 @@ ymaps.ready(function () {
   myMap.geoObjects.add(myPlacemark);
 });
 
-// swiper
+
+let currentSlidep = 0;
+const slidesP = document.querySelectorAll('.slideP');
+
+function showSlide(n) {
+    slidesP[currentSlide].classList.remove('activeP');
+    currentSlide = (n + slidesP.length) % slidesP.length;
+    slidesP[currentSlide].classList.add('activeP');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    showSlide(currentSlide);
+
+    setInterval(function() {
+        showSlide(currentSlide + 1);
+    }, 3000); // Интервал смены слайдов (в миллисекундах)
+});
+
+// slider
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    showSlide(currentSlide);
+
+    setInterval(function() {
+        showSlide(currentSlide + 1);
+    }, 3000); // Интервал смены слайдов (в миллисекундах)
+});
