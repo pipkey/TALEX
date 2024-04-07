e858a3f88a0f51f38846854674b35bdd655a9e29
pipkey <galagen2014@gmail.com>
2024-04-07 23:57:18 +0300

    sliders without footer

--- js/index.js
+++ js/index.js
@@ -13,23 +13,45 @@ ymaps.ready(function () {
 });
 
 
-let currentSlidep = 0;
 const slidesP = document.querySelectorAll('.slideP');
+const dotsContainerP = document.querySelector('.paginationP');
+let intervalId;
 
-function showSlide(n) {
-    slidesP[currentSlide].classList.remove('activeP');
-    currentSlide = (n + slidesP.length) % slidesP.length;
-    slidesP[currentSlide].classList.add('activeP');
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    showSlide(currentSlide);
-
-    setInterval(function() {
-        showSlide(currentSlide + 1);
-    }, 3000); // Интервал смены слайдов (в миллисекундах)
+slidesP.forEach((slideP, index) => {
+  const dotP = document.createElement('spanP');
+  dotP.classList.add('dotP');
+  if (index === 0) {
+    dotP.classList.add('activeP');
+  }
+  dotP.addEventListener('click', () => {
+    goToSlide(index);
+  });
+  dotsContainerP.appendChild(dotP);
 });
 
+let currentSlideP = 0;
+
+function goToSlide(slideIndexP) {
+  slidesP[currentSlideP].classList.remove('activeP');
+  slidesP[slideIndexP].classList.add('activeP');
+  dotsContainerP.querySelector('.dotP.activeP').classList.remove('activeP');
+  dotsContainerP.querySelectorAll('.dotP')[slideIndexP].classList.add('activeP');
+  currentSlideP = slideIndexP;
+}
+function nextSlide() {
+  const nextSlideIndex = (currentSlideP + 1) % slidesP.length;
+  goToSlide(nextSlideIndex);
+}
+
+function startSlider() {
+  intervalId = setInterval(nextSlide, 3000); // Прокрутка каждые 5 секунд
+}
+
+function stopSlider() {
+  clearInterval(intervalId);
+}
+
+startSlider();
 // slider
 
 let currentSlide = 0;
